942f04404334bc7addf07e389866a675721d4287
Jason Crockett <JesusisLord33333@outlook.com>
2022-12-21 15:03:43 +0000

    added browser

--- main.rb
+++ main.rb
@@ -1,13 +1,18 @@
 require 'sinatra'
 
+
 class CuratedLegislation < Sinatra::Base
   get '/' do
     erb :index
   end
-  get '/assets/application.js' do
-    send_file('app/assets/application.js')
+  get /\/id\/(ukpga|ukla|ukppa|gbla|apgb|gbppa|aep|aosp|asp|aip|apni|mnia|nia|ukcm|mwa|anaw|asc|uksi|ssi|wsi|nisr|ukci|nisi|ukmo|nisro|ukdsi|sdsi|nidsr|nidsi|wdsi)\/\d+\/\d+\/\d+/ do
+    erb :article, locals: {path: request["PATH_INFO"]}
   end
-  get /^\/id\/(ukpga|ukla|ukppa|gbla|apgb|gbppa|aep|aosp|asp|aip|apni|mnia|nia|ukcm|mwa|anaw|asc|uksi|ssi|wsi|nisr|ukci|nisi|ukmo|nisro|ukdsi|sdsi|nidsr|nidsi|wdsi)\/\d+\/\d+\/\d+$/ do
+  get /\/(ukpga|ukla|ukppa|gbla|apgb|gbppa|aep|aosp|asp|aip|apni|mnia|nia|ukcm|mwa|anaw|asc|uksi|ssi|wsi|nisr|ukci|nisi|ukmo|nisro|ukdsi|sdsi|nidsr|nidsi|wdsi)\/.*/ do
+    erb :browser,locals: {path: request["PATH_INFO"]}
+  end
+  get /\/browse\/.*/ do
+    erb :browser,locals: {path: request["PATH_INFO"]}
+  end
 
-  end
 end
