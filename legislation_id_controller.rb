3b163cd3acb969b45bb7e8c5c117f22ec66b6c19
Jason Crockett <JesusisLord33333@outlook.com>
2022-12-21 15:03:23 +0000

    getting articles

--- legislation_id_controller.rb
+++ legislation_id_controller.rb
@@ -2,15 +2,20 @@ require 'httparty'
 class LegislationIdController
   include HTTParty
   base_uri 'www.legislation.gov.uk'
-  def initialize(title=nil,type=nil,year=nil,number=nil)
+  def initialize(title=nil,type=nil,year=nil,number=nil,section=nil)
     @title=URI.encode_www_form_component(title)
     @type=URI.encode_www_form_component(type)
     @year=URI.encode_www_form_component(year)
     @number=URI.encode_www_form_component(number)
+    @section=URI.encode_www_form_component(section)
     @options = {query:{title:@title,number:@number,type:@type,year:@year}}
     @options.delete_if(){|k,v| v.nil?}
   end
-  def get
+  def search
     self.class.get('/id',@options)
   end
+  def get_article
+    self.class.get("/id/#{type}/#{@year}/#{@number}/#{@section}")
+  end
+
 end
